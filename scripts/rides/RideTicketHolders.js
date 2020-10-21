bc8565c3b8c2c53923b199e5ab07fbfd21900969
Jasmin Kaset <kasetski@gmail.com>
2020-10-21 11:40:03 -0500

    RideTicketHolders.js function working

--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -2,7 +2,23 @@ const contentTarget = document.querySelector(".rides")
 const eventHub = document.querySelector("#state-fair")
 
 export const RideTicketHolders = () => {
-    eventHub.addEventListener("", customEvent => {
+    eventHub.addEventListener("rideTicketPurchased", event => { const rideColor = event.detail.clickedButton
+        contentTarget.classList.add(rideColor)
+        contentTarget.innerHTML += `
+            <div class="person rider">
+            </div>
+        `
     })
 }
 
+// Add an event listener to the event hub that listens for the custom event that you dispatched from the TicketBooth component.
+
+
+// In the body of the event listener callback function, add a new <div> element to the content target that has the correct classes on it (see above).
+
+// eventHub.addEventListener("colorChosen", event => {
+//     const color = event.detail.color
+
+//     contentTarget.classList = []
+//     contentTarget.classList.add(color)
+// })
