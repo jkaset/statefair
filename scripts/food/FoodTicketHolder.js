491c3cf62db8e9f907c1759829a76bd7f81c37be
Jasmin Kaset <kasetski@gmail.com>
2020-10-21 15:25:47 -0500

    added food ticket info

---
+++ scripts/food/FoodTicketHolder.js
@@ -0,0 +1,15 @@
+// and add an event listener to the event hub that listens for the food ticket custom event that you will dispatch from the TicketBooth component.
+const contentTarget = document.querySelector(".food")
+const eventHub = document.querySelector("#state-fair")
+
+export const FoodTicketHolders = () => {
+    eventHub.addEventListener("foodTicketPurchased", event => { const foodClicked = event.detail.clickedButton
+        contentTarget.classList.add(foodClicked)
+        contentTarget.innerHTML += `
+            <div class="person eater">
+            </div>
+        `
+    })
+}
+
+// In the body of the event listener callback function, add a new <div> element to the content target that has the correct classes on it (see above).
